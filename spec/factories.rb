c20442998a838ef8565e2eea3228455cbddad7c7
Nicholas Wargnier <nwargnier@gmail.com>
2013-11-21 22:39:44 -0800

    delete file commit

--- spec/factories.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    username 'nwargnier'
-    email { Faker::Internet.email.capitalize }
-    password 'password'
-    password_confirmation 'password'
-  end
-end
